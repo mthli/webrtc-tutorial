5cc6899746c929d6e3009b57f545c29c1ce55e8f
mthli <matthewlee0725@gmail.com>
2021-02-08 19:45:48 +0800

    refactor: replace favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,28 @@
 module.exports = {
-  pathPrefix: '/docs',
+  pathPrefix: '/',
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `WebRTC 学习指南`,
+        short_name: `WebRTC 学习指南`,
+        start_url: `/`,
+        display: 'minimal-ui',
+        icon: 'content/assets/profile.jpg',
+      },
+    },
     {
       resolve: 'gatsby-theme-apollo-docs',
       options: {
         root: __dirname,
-        baseDir: 'docs',
-        contentDir: 'source',
+        baseDir: '/',
+        contentDir: 'content',
 
         siteName: 'WebRTC 学习指南',
         pageTitle: 'WebRTC 学习指南',
         description: 'Learning WebRTC the Hard Way 👀',
         githubRepo: 'mthli/webrtc-tutorial',
+        twitterHandle: 'mth_li',
 
         sidebarCategories: {
           null: [
