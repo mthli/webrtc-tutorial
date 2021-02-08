830ecfe25502f2ca514e54eaaf799f522d2abadc
mthli <matthewlee0725@gmail.com>
2021-02-08 22:08:04 +0800

    refactor: change mobile logo

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -4,15 +4,16 @@ import styled from '@emotion/styled';
 import { withPrefix } from 'gatsby';
 
 const Wrapper = styled.div({
-  display: "flex",
+  display: 'flex',
+  marginTop: '-4px',
 });
 
 export default function Logo() {
   return (
     <Wrapper>
       <img
-        style={{ width: '24px' }}
-        src={withPrefix('/icons/icon-48x48.png')}
+        style={{ width: '32px' }}
+        src={withPrefix('/icons/icon-96x96.png')}
         alt={'logo'}
       />
     </Wrapper>
---
+++ src/gatsby-theme-apollo-docs/components/mobile-logo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import { withPrefix } from 'gatsby';
+
+const Wrapper = styled.div({
+  display: 'flex',
+  marginRight: '-12px',
+});
+
+class MobileLogo extends React.PureComponent {
+  render() {
+    return (
+      <Wrapper>
+        <img
+          style={{
+            margin: 0,
+            width: '32px',
+           }}
+          src={withPrefix('/icons/icon-96x96.png')}
+          alt={'logo'}
+        />
+      </Wrapper>
+    );
+  }
+}
+
+export { MobileLogo }
