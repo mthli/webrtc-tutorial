635cb06c7ab025b08aa945ec0fa3cdbe45c8ceab
mthli <matthewlee0725@gmail.com>
2021-05-04 11:42:17 +0800

    chore: add bytedance-verification-code

--- src/@mthli/gatsby-theme-apollo-docs/components/custom-seo.js
+++ src/@mthli/gatsby-theme-apollo-docs/components/custom-seo.js
@@ -8,6 +8,7 @@ export default function CustomSEO({ image, baseUrl, twitterHandle, ...props }) {
   return (
     <SEO {...props} twitterCard="summary_large_image" favicon="/favicon-32x32.png">
       <meta name="google-site-verification" content="-qgu2UvNRmKmA2nlvPii0Ij_5KvDP7CoiPrrrt7fCDw" />
+      <meta name="bytedance-verification-code" content="Di2CvrKo5GQhq+QNFxJc" />
       {baseUrl && <meta property="og:image" content={baseUrl + imagePath} />}
       {baseUrl && <meta name="twitter:image" content={baseUrl + imagePath} />}
       {twitterHandle && <meta name="twitter:site" content={`@${twitterHandle}`} />}
