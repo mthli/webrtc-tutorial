c2f56e69e8fd9d033e3b24b71eeff40d7febed10
mthli <matthewlee0725@gmail.com>
2021-05-04 11:57:18 +0800

    chore: add bytedance search script

--- src/@mthli/gatsby-theme-apollo-docs/components/custom-seo.js
+++ src/@mthli/gatsby-theme-apollo-docs/components/custom-seo.js
@@ -9,6 +9,17 @@ export default function CustomSEO({ image, baseUrl, twitterHandle, ...props }) {
     <SEO {...props} twitterCard="summary_large_image" favicon="/favicon-32x32.png">
       <meta name="google-site-verification" content="-qgu2UvNRmKmA2nlvPii0Ij_5KvDP7CoiPrrrt7fCDw" />
       <meta name="bytedance-verification-code" content="Di2CvrKo5GQhq+QNFxJc" />
+      <script>
+        {`
+          (function(){
+            var el = document.createElement("script");
+            el.src = "https://sf1-scmcdn-tos.pstatp.com/goofy/ttzz/push.js?640b72aa3e4fdf9ffd1a9a05fc714e9cdddc3da3a07c6d62b917ee7736a67eed8d4676f17aecfe4838ef223da837021a5615cc802677690824ac7c88ac0f6a982b8d7c8c6655c9b00211740aa8a98e2e";
+            el.id = "ttzz";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(el, s);
+          })(window)
+        `}
+      </script>
       {baseUrl && <meta property="og:image" content={baseUrl + imagePath} />}
       {baseUrl && <meta name="twitter:image" content={baseUrl + imagePath} />}
       {twitterHandle && <meta name="twitter:site" content={`@${twitterHandle}`} />}
